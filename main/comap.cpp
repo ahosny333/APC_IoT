e1f670d232e4c03d3c2c25935029b094be8a2d5d
ahmed hosny <ahmed.hosny@the-diy-life.co>
2025-11-02 16:47:50 +0200

    add states for reading and writing to comap board

--- main/comap.cpp
+++ main/comap.cpp
@@ -12,6 +12,14 @@ extern struct dse_modbus_data dse_data;
 extern bool dse_modbus_success;
 extern uint16_t dse_scan_interval;
 extern uint32_t dse_scan_timer;
+extern uint8_t dse_request_started;
+extern bool start_write;
+extern char command_name[30];
+extern uint8_t command_value;
+
+enum comap_task_status { read_general=0,read_mode, write_command };
+enum comap_task_status comap_state = read_general;
+enum comap_task_status last_comap_state = read_general;
 
 
 void comap_get_values()
@@ -45,7 +53,7 @@ bool comap_cb(Modbus::ResultCode event, uint16_t transactionId, void* data) {
   if (event != Modbus::EX_SUCCESS) {
     // Serial.print("Request result: 0x");
     // Serial.print(event, HEX);
-    // dse_request_started = 0;
+    dse_request_started = 0;
     //dse_scan_timer = millis();
     dse_modbus_success = false;
     memset(modbus_string,0,sizeof(modbus_string));
@@ -56,8 +64,26 @@ bool comap_cb(Modbus::ResultCode event, uint16_t transactionId, void* data) {
   else{
     // Serial.print("success Request result: 0x");
     dse_modbus_success = true;
-    comap_get_values();
+    if (comap_state == read_general){
+      comap_get_values();
+      dse_request_started = 0;
+      comap_state = read_mode;
+    }
+     else if (comap_state == read_mode){
+      if(registers[0] ==0x02FE )
+      {dse_data.control_mode = 0;}
+      else if(registers[0] == 0x01FF)
+      {dse_data.control_mode = 1;}
+      dse_request_started = 0;
+      comap_state = read_general;
+      dse_scan_timer = millis();
+    }
 
+  }
+  if(comap_state == write_command)
+  {
+    dse_request_started = 0;
+    comap_state = last_comap_state;
   }
   return true;
 
@@ -66,13 +92,79 @@ bool comap_cb(Modbus::ResultCode event, uint16_t transactionId, void* data) {
 
 void comap_read_modbus()
 {
-  if(millis() - dse_scan_timer >= dse_scan_interval )
+  if(start_write && dse_request_started == 0 && !dse_rtu.slave()) 
   {
-    dse_rtu.readHreg(SLAVE_ID, 1000, registers,41, comap_cb);
-    dse_scan_timer = millis();
+      start_write = false;
+      if((strcmp(command_name,"Stop mode") == 0) && command_value == 1) {
+          write_registers[0] =  0x02FD;
+          write_registers[1] = 0x00;
+          write_registers[2] = 0x01;
+          last_comap_state = comap_state;
+          comap_state = write_command;
+        }
+        else if((strcmp(command_name,"Auto mode") == 0) && command_value == 1) {
+          write_registers[0] =  0x01FE;
+          write_registers[1] = 0x00;
+          write_registers[2] = 0x01;
+          last_comap_state = comap_state;
+          comap_state = write_command;
+        }
+        if(comap_state == write_command)
+        {
+          dse_request_started = 1;
+          // DEBUG_PRINTLN("Write state");
+          // DEBUG_PRINTLN(command_name);
+          // DEBUG_PRINTLN(command_value);
+          dse_rtu.writeHreg(SLAVE_ID,4207,write_registers,3,comap_cb);
+        }
+
+
   }
+  else{
+    
+    if(millis() - dse_scan_timer >= dse_scan_interval )
+    {
+      switch (comap_state)
+          {
+            case read_general:
+              if(dse_request_started == 0 && !dse_rtu.slave()) 
+              {
+                dse_request_started = 1;
+                dse_rtu.readHreg(SLAVE_ID, 1000, registers,41, comap_cb);
 
+              }
+              break;
 
+            case read_mode:
+              if(dse_request_started == 0 && !dse_rtu.slave()) 
+              {
+                dse_request_started = 1;
+                dse_rtu.readHreg(SLAVE_ID, 4208, registers,1, comap_cb);
+
+              }
+              break;
+                    
+            default:
+              break;
+          }
+    }
+  }
 }
+
+
+
+
+
+
+
+//     if(dse_request_started == 0 && !dse_rtu.slave()){ 
+//       dse_request_started =1;
+//       dse_rtu.readHreg(SLAVE_ID, 1000, registers,41, comap_cb);
+//       //dse_scan_timer = millis();
+//     }
+//   }
+
+
+// }
 #endif
 
